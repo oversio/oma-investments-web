723e1a4a99bce258556453a627949db489e9da93
Over Martinez <overmartinez@gmail.com>
2024-02-16 00:51:48 -0300

    feat(routes): adjust routes and among other things

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,19 +1,37 @@
 import { NextUIProvider } from "@nextui-org/react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Outlet, Route, Routes, useNavigate } from "react-router-dom";
+
+import { queryClient } from "../common/api/generate-query-client";
 import { Layout } from "../common/components/layout/layout";
+import { ToastContainer } from "../common/components/toast/components/toast-container";
+import { CompanyListPage } from "../features/companies/features/company-list/company-list-page";
+import { DashboardPage } from "../features/dashboard/dashboard-page";
+import { SettingsPage } from "../features/settings/settings-page";
 
 export function App() {
   const navigate = useNavigate();
 
   return (
-    <NextUIProvider navigate={navigate}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="" element={<h1>Dashboard</h1>} />
-          <Route path="companies" element={<h1>Companies</h1>} />
-          <Route path="settings" element={<h1>Settings</h1>} />
-        </Route>
-      </Routes>
-    </NextUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider navigate={navigate}>
+        <Routes>
+          <Route
+            element={
+              <>
+                <Outlet />
+                <ToastContainer />
+              </>
+            }
+          >
+            <Route path="/" element={<Layout />}>
+              <Route path="" element={<DashboardPage />} />
+              <Route path="companies" element={<CompanyListPage />} />
+              <Route path="settings" element={<SettingsPage />} />
+            </Route>
+          </Route>
+        </Routes>
+      </NextUIProvider>
+    </QueryClientProvider>
   );
 }
